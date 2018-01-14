3810c4472c0cf3401fe546f281ac37a776819c75
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-14 10:23:07 -0200

    created OffsetDateTimeJsonDeserialize

---
+++ src/main/java/org/frekele/cielo/lio/remote/client/converter/OffsetDateTimeJsonDeserialize.java
@@ -0,0 +1,28 @@
+package org.frekele.cielo.lio.remote.client.converter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class OffsetDateTimeJsonDeserialize extends StdDeserializer<OffsetDateTime> {
+
+    protected OffsetDateTimeJsonDeserialize() {
+        super(OffsetDateTime.class);
+    }
+
+    @Override
+    public OffsetDateTime deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        if (jsonParser != null) {
+            String value = jsonParser.readValueAs(String.class);
+            if (value != null) {
+                return OffsetDateTime.parse(value, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            }
+        }
+        return null;
+    }
+}
