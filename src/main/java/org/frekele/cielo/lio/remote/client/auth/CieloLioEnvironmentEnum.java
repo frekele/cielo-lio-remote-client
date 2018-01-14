8984fa7b8f4797c246310e3fc08e4b48486f0939
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-14 12:07:39 -0200

    CieloLioEnvironmentEnum

---
+++ src/main/java/org/frekele/cielo/lio/remote/client/auth/CieloLioEnvironmentEnum.java
@@ -0,0 +1,11 @@
+package org.frekele.cielo.lio.remote.client.auth;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public enum CieloLioEnvironmentEnum {
+
+    PRODUCTION,
+    SANDBOX;
+
+}
