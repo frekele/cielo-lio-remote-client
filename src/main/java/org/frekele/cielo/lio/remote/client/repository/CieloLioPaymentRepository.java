64e8973ae0bf22b13ae77438c39f71643fdede88
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-14 10:31:19 -0200

    created CieloLioPaymentRepository

---
+++ src/main/java/org/frekele/cielo/lio/remote/client/repository/CieloLioPaymentRepository.java
@@ -0,0 +1,7 @@
+package org.frekele.cielo.lio.remote.client.repository;
+
+import java.io.Serializable;
+
+public interface CieloLioPaymentRepository extends Serializable {
+
+}
---
+++ src/main/java/org/frekele/cielo/lio/remote/client/repository/CieloLioPaymentRepositoryImpl.java
@@ -0,0 +1,6 @@
+package org.frekele.cielo.lio.remote.client.repository;
+
+public class CieloLioPaymentRepositoryImpl implements CieloLioPaymentRepository {
+
+    private static final long serialVersionUID = 1L;
+}
