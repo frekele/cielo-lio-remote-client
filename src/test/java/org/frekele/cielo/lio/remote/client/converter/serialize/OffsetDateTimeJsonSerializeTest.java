6ae373822bed04ffc631f64ece589611006258c5
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 15:59:45 -0200

    OffsetDateTimeJsonSerializeTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/converter/serialize/OffsetDateTimeJsonSerializeTest.java
@@ -0,0 +1,34 @@
+package org.frekele.cielo.lio.remote.client.converter.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.OffsetDateTime;
+
+import static org.testng.Assert.*;
+
+public class OffsetDateTimeJsonSerializeTest {
+
+    @Mock
+    JsonGenerator jsonGenerator;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        OffsetDateTimeJsonSerialize jsonSerialize = new OffsetDateTimeJsonSerialize();
+        assertNotNull(jsonSerialize);
+
+        jsonSerialize.serialize(null, null, null);
+        jsonSerialize.serialize(null, jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now(), jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now().minusDays(4), jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now().plusHours(6), jsonGenerator, null);
+    }
+}
