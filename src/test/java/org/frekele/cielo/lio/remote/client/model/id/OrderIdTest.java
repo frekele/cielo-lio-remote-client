6fd812e06dd0d37f2cf2e4f2be8fb8874e4dc6bb
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 13:56:04 -0200

    OrderIdTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/model/id/OrderIdTest.java
@@ -0,0 +1,22 @@
+package org.frekele.cielo.lio.remote.client.model.id;
+
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class OrderIdTest {
+
+    @Test
+    public void testNewInstance() throws Exception {
+        String id = "af9658-c3dd-49fa-acf29c38-2aa7aa89a";
+
+        OrderId entity = new OrderId();
+        entity.setId(id);
+
+        assertNotNull(entity);
+        assertEquals(id, entity.getId());
+    }
+}
