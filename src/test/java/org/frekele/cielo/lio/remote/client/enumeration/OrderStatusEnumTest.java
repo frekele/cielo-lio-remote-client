2799165d996c4971130e61dc50f17b66e0c191d4
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 13:18:38 -0200

    OrderStatusEnumTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/enumeration/OrderStatusEnumTest.java
@@ -0,0 +1,42 @@
+package org.frekele.cielo.lio.remote.client.enumeration;
+
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class OrderStatusEnumTest {
+
+    @Test
+    public void testFromValue() throws Exception {
+        OrderStatusEnum environment = OrderStatusEnum.fromValue("DRAFT");
+        assertNotEquals(null, environment);
+        environment = OrderStatusEnum.fromValue("ENTERED");
+        assertNotEquals(null, environment);
+        environment = OrderStatusEnum.fromValue("RE_ENTERED");
+        assertNotEquals(null, environment);
+        environment = OrderStatusEnum.fromValue("PAID");
+        assertNotEquals(null, environment);
+        environment = OrderStatusEnum.fromValue("CANCELED");
+        assertNotEquals(null, environment);
+        environment = OrderStatusEnum.fromValue("CLOSED");
+        assertNotEquals(null, environment);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        List<OrderStatusEnum> listAll = OrderStatusEnum.getAll();
+        assertNotEquals(null, listAll);
+        assertEquals(listAll.size(), 6);
+        assertEquals(listAll.contains(OrderStatusEnum.DRAFT), true);
+        assertEquals(listAll.contains(OrderStatusEnum.ENTERED), true);
+        assertEquals(listAll.contains(OrderStatusEnum.RE_ENTERED), true);
+        assertEquals(listAll.contains(OrderStatusEnum.PAID), true);
+        assertEquals(listAll.contains(OrderStatusEnum.CANCELED), true);
+        assertEquals(listAll.contains(OrderStatusEnum.CLOSED), true);
+    }
+}
