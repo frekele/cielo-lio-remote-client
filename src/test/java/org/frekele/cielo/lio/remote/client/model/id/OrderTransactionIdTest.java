0d846baebfa0912d4deab399425f3b03e32697b2
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 13:56:18 -0200

    OrderTransactionIdTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/model/id/OrderTransactionIdTest.java
@@ -0,0 +1,22 @@
+package org.frekele.cielo.lio.remote.client.model.id;
+
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class OrderTransactionIdTest {
+
+    @Test
+    public void testNewInstance() throws Exception {
+        String id = "a32aa8-c3dd-49fa-58cf29c-7aaaf9689a";
+
+        OrderTransactionId entity = new OrderTransactionId();
+        entity.setId(id);
+
+        assertNotNull(entity);
+        assertEquals(id, entity.getId());
+    }
+}
