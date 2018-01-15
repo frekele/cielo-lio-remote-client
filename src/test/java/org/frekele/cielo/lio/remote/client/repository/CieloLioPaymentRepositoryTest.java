34f9c7b4a6f730d37b6ffd95fa48430d922398d4
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 12:09:57 -0200

    improvement test for OrderItemEntity.

--- src/test/java/org/frekele/cielo/lio/remote/client/repository/CieloLioPaymentRepositoryTest.java
+++ src/test/java/org/frekele/cielo/lio/remote/client/repository/CieloLioPaymentRepositoryTest.java
@@ -8,6 +8,7 @@ import org.frekele.cielo.lio.remote.client.enumeration.OrderStatusEnum;
 import org.frekele.cielo.lio.remote.client.model.OrderEntity;
 import org.frekele.cielo.lio.remote.client.model.OrderItemEntity;
 import org.frekele.cielo.lio.remote.client.model.id.OrderId;
+import org.frekele.cielo.lio.remote.client.model.id.OrderItemId;
 import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
@@ -29,9 +30,13 @@ public class CieloLioPaymentRepositoryTest {
 
     private ObjectMapper mapper = new ObjectMapper();
 
+    private OrderId orderId;
+
     private OrderEntity order;
 
-    private OrderId orderId;
+    private OrderItemId orderItemId2;
+
+    private OrderItemEntity orderItem2;
 
     @BeforeClass
     public void init() throws Exception {
@@ -48,14 +53,14 @@ public class CieloLioPaymentRepositoryTest {
         order.setNumber("12345");
         order.setReference("PEDIDO #12345");
         order.setNotes("Cliente Fulano de Tal");
-        order.setPrice(BigDecimal.valueOf(125.34));
+        order.setPrice(BigDecimal.valueOf(325.34));
         order.setItems(new ArrayList<>());
         OrderItemEntity item = new OrderItemEntity();
         item.setSku("RTG-234-AQF-6587-C57");
         item.setName("Mesa de Formica Branca");
         item.setQuantity(1);
         item.setUnitOfMeasure("UN");
-        item.setUnitPrice(BigDecimal.valueOf(125.34));
+        item.setUnitPrice(BigDecimal.valueOf(325.34));
         order.getItems().add(item);
 
         System.out.println("new OrderCieloEntity");
@@ -65,7 +70,6 @@ public class CieloLioPaymentRepositoryTest {
     @Test
     public void testOrderPost() throws Exception {
         orderId = repository.orderPost(order);
-        System.out.println("before orderPost");
         System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderId));
     }
 
@@ -76,6 +80,47 @@ public class CieloLioPaymentRepositoryTest {
     }
 
     @Test(dependsOnMethods = "testOrderPut")
+    public void testOrderPostItem() throws Exception {
+        orderItem2 = new OrderItemEntity();
+        orderItem2.setSku("XPT-456-564-34554-3453");
+        orderItem2.setName("Cadeira de Madeira Branca");
+        orderItem2.setQuantity(4);
+        orderItem2.setUnitOfMeasure("UN");
+        orderItem2.setUnitPrice(BigDecimal.valueOf(103.10));
+        order.getItems().add(orderItem2);
+        BigDecimal orderPrice = orderItem2.getUnitPrice().multiply(BigDecimal.valueOf(orderItem2.getQuantity())).add(order.getPrice());
+        order.setPrice(orderPrice);
+        System.out.println("new OrderItemEntity");
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderItem2));
+
+        orderItemId2 = repository.orderPostItem(orderId, orderItem2);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderId));
+    }
+
+    @Test(dependsOnMethods = "testOrderPostItem")
+    public void testOrderPutItem() throws Exception {
+        orderItem2.setUnitOfMeasure("CX");
+        repository.orderPutItem(orderId, orderItemId2, orderItem2);
+    }
+
+    @Test(dependsOnMethods = "testOrderPutItem")
+    public void testOrderGetItem() throws Exception {
+        OrderItemEntity orderItemResult = repository.orderGetItem(orderId, orderItemId2);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderItemResult));
+    }
+
+    @Test(dependsOnMethods = "testOrderGetItem")
+    public void testOrderGetItems() throws Exception {
+        List<OrderItemEntity> resultList = repository.orderGetItems(orderId);
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultList));
+    }
+
+    @Test(dependsOnMethods = "testOrderGetItems")
+    public void testOrderDeleteItem() throws Exception {
+        repository.orderDeleteItem(orderId, orderItemId2);
+    }
+
+    @Test(dependsOnMethods = "testOrderDeleteItem")
     public void testOrderPutOperation() throws Exception {
         repository.orderPutOperation(orderId, OperationEnum.PLACE);
     }
@@ -87,6 +132,18 @@ public class CieloLioPaymentRepositoryTest {
     }
 
     @Test(dependsOnMethods = "testOrderGet")
+    public void testOrderPostTransaction() throws Exception {
+    }
+
+    @Test(dependsOnMethods = "testOrderPostTransaction")
+    public void testOrderGetTransactionById() throws Exception {
+    }
+
+    @Test(dependsOnMethods = "testOrderGetTransactionById")
+    public void testOrderGetTransactions() throws Exception {
+    }
+
+    @Test(dependsOnMethods = "testOrderGetTransactions")
     public void testOrderGetByNumber() throws Exception {
         List<OrderEntity> resultList = repository.orderGetByNumber("12345");
         System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultList));
@@ -105,45 +162,13 @@ public class CieloLioPaymentRepositoryTest {
     }
 
     @Test(dependsOnMethods = "testOrderGetByStatus")
-    public void testOrderDelete() throws Exception {
-        repository.orderDelete(orderId);
-    }
-
-    @Test(dependsOnMethods = "testOrderDelete")
     public void testOrderGetAll() throws Exception {
         List<OrderEntity> resultList = repository.orderGetAll();
         System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resultList));
     }
 
-//    @Test
-//    public void testOrderGetItems() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderGetItem() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderPostItem() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderPutItem() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderDeleteItem() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderGetTransactions() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderGetTransactionById() throws Exception {
-//    }
-//
-//    @Test
-//    public void testOrderPostTransaction() throws Exception {
-//    }
+    @Test(dependsOnMethods = "testOrderGetAll")
+    public void testOrderDelete() throws Exception {
+        repository.orderDelete(orderId);
+    }
 }
