b885ed448536b5103a70fd3f5fd6c3a2e5709fdc
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 15:54:49 -0200

    BigDecimalJsonDeserializeTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/converter/deserialize/BigDecimalJsonDeserializeTest.java
@@ -0,0 +1,44 @@
+package org.frekele.cielo.lio.remote.client.converter.deserialize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class BigDecimalJsonDeserializeTest {
+
+    @Mock
+    private JsonParser jsonParser;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDeserialize() throws Exception {
+        BigDecimalJsonDeserialize jsonDeserialize = new BigDecimalJsonDeserialize();
+        assertNotNull(jsonDeserialize);
+
+        BigDecimal resultValue = jsonDeserialize.deserialize(null, null);
+        assertNull(resultValue);
+
+        when(jsonParser.readValueAs(Long.class)).thenReturn(null);
+        resultValue = jsonDeserialize.deserialize(jsonParser, null);
+        assertNull(resultValue);
+
+        long longValue = 5432;
+        when(jsonParser.readValueAs(Long.class)).thenReturn(longValue);
+        resultValue = jsonDeserialize.deserialize(jsonParser, null);
+        assertEquals(BigDecimal.valueOf(longValue, 2), resultValue);
+    }
+}
