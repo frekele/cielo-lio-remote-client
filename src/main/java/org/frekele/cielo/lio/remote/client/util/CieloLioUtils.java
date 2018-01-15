5ce175d15af24176daadb4b0a2cd1c8e19aa5158
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 15:28:46 -0200

    throwInjection only  throw new CieloLioException if contains args.

--- src/main/java/org/frekele/cielo/lio/remote/client/util/CieloLioUtils.java
+++ src/main/java/org/frekele/cielo/lio/remote/client/util/CieloLioUtils.java
@@ -5,13 +5,11 @@ import org.frekele.cielo.lio.remote.client.exception.CieloLioException;
 /**
  * @author frekele - Leandro Kersting de Freitas
  */
-public class CieloLioUtils {
+public final class CieloLioUtils {
 
     public static void throwInjection(Object... objects) {
         String msg = "Parameters in the constructor were not injected!";
-        if (objects == null) {
-            throw new CieloLioException(msg);
-        } else {
+        if (objects != null) {
             for (Object obj : objects) {
                 if (obj == null) {
                     throw new CieloLioException(msg);
