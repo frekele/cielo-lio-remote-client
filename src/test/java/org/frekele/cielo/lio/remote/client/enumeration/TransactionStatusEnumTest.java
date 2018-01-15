360109086b3efdb6bf2fb4b3521b563eea44e553
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 13:18:45 -0200

    TransactionStatusEnumTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/enumeration/TransactionStatusEnumTest.java
@@ -0,0 +1,33 @@
+package org.frekele.cielo.lio.remote.client.enumeration;
+
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class TransactionStatusEnumTest {
+
+    @Test
+    public void testFromValue() throws Exception {
+        TransactionStatusEnum environment = TransactionStatusEnum.fromValue("CONFIRMED");
+        assertNotEquals(null, environment);
+        environment = TransactionStatusEnum.fromValue("PENDING");
+        assertNotEquals(null, environment);
+        environment = TransactionStatusEnum.fromValue("CANCELLED");
+        assertNotEquals(null, environment);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        List<TransactionStatusEnum> listAll = TransactionStatusEnum.getAll();
+        assertNotEquals(null, listAll);
+        assertEquals(listAll.size(), 3);
+        assertEquals(listAll.contains(TransactionStatusEnum.CONFIRMED), true);
+        assertEquals(listAll.contains(TransactionStatusEnum.PENDING), true);
+        assertEquals(listAll.contains(TransactionStatusEnum.CANCELLED), true);
+    }
+}
