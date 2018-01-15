6556105e64854d3879b5edb0c8f4a35c40b0ccaa
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-01-15 13:40:31 -0200

    CieloLioExceptionTest

---
+++ src/test/java/org/frekele/cielo/lio/remote/client/exception/CieloLioExceptionTest.java
@@ -0,0 +1,35 @@
+package org.frekele.cielo.lio.remote.client.exception;
+
+import org.frekele.cielo.lio.remote.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+@Listeners(InvokedMethodListener.class)
+public class CieloLioExceptionTest {
+
+    String msg = "Error Message";
+
+    IOException cause = new IOException("IO error");
+
+    @Test(expectedExceptions = CieloLioException.class)
+    public void testConstructorWithMessage() throws Exception {
+        throw new CieloLioException(msg);
+    }
+
+    @Test(expectedExceptions = CieloLioException.class)
+    public void testConstructorWithCause() throws Exception {
+        throw new CieloLioException(cause);
+    }
+
+    @Test(expectedExceptions = CieloLioException.class)
+    public void testConstructorWithMessageAndCause() throws Exception {
+        throw new CieloLioException(msg, cause);
+    }
+
+    @Test(expectedExceptions = CieloLioException.class)
+    public void testConstructorWithMessageAndCauseAndMore() throws Exception {
+        throw new CieloLioException(msg, cause, true, true);
+    }
+}
